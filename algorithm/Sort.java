e8372ed31bdb818b0eea4cc720fa8c9a1ece837a
michael <michael@mini.fritz.box>
2017-01-07 08:02:00 +0100

    Basic skeleton

---
+++ algorithm/Sort.java
@@ -0,0 +1,49 @@
+
+package algorithm;
+
+public class Sort
+{
+    void bubbleSort(int [] list )
+    {
+        
+    }
+    
+    void bubbleSort(int [] list, int leftIdx, int rightIdx)
+    {
+        
+    }
+    
+    void bubbleUp(int[] list, int leftIdx, int i)
+    {
+        
+    }
+    
+    void quickSort(int [] list)
+    {
+        
+    }
+    
+    void quickSort(int [] list, int leftIdx, int rightIdx)
+    {
+        
+    }
+    
+    int divide(int [] list, int leftIdx, int rightIdx)
+    {
+        return 1;
+    }
+    
+    void swap(int[] list, int i, int j)
+    {
+    }
+    
+    String toString(int [] list, int start, int end)
+    {
+        return "";
+    }
+    
+    String toString(int [] list)
+    {
+        return "";
+    }
+}
