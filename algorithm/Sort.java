fb08e728b78eb1cf5b2673297f9b130bba7c4165
michael <michael@mini.fritz.box>
2017-01-08 10:15:03 +0100

    Implementing QuickSort 1

--- algorithm/Sort.java
+++ algorithm/Sort.java
@@ -8,6 +8,12 @@ public class Sort
         Sort.bubbleSort(list, 0, list.length - 1);
     }
     
+    /*
+        Implement the BubbleSort algorithm.
+        One as to specify a lower and an upper index.
+        These range of the array is then sorted by the algorithm.
+        The lower and the upper index have to be checked for validity.
+    */
     public static void bubbleSort(int[] list, int leftIdx, int rightIdx)
             throws IllegalArgumentException
     {
@@ -35,6 +41,10 @@ public class Sort
         }
     }
     
+    // The largest element within the specified array-range
+    // has to be moved to position i.
+    // Basically just the inner loop of BubbleSort.
+    // Parameter validity checks required.
     public static void bubbleUp(int[] list, int leftIdx, int i)
     {
         if (leftIdx < 0 || leftIdx > list.length - 1 
@@ -57,21 +67,80 @@ public class Sort
         }
     }
     
-    public static void quickSort(int [] list)
+    public static void quickSort(int[] list)
     {
-        
+        Sort.quickSort(list, 0, list.length);
     }
     
-    public static void quickSort(int [] list, int leftIdx, int rightIdx)
+    // Validate leftIndex and rightIndex concerning being within the
+    //  array range.
+    
+    public static void quickSort(int[] list, int leftIdx, int rightIdx)
     {
+        if (leftIdx < 0 || leftIdx > list.length - 1 
+                || rightIdx < 0 || rightIdx > list.length - 1) 
+        {
+            throw new IllegalArgumentException("Left or right index invalid.");
+        }
         
+        int index = divide(list, leftIdx, rightIdx);
+        
+        if (leftIdx < index - 1) 
+        {
+            quickSort(list, leftIdx, index - 1);
+        }
+        
+        if (index < rightIdx) 
+        {
+            quickSort(list, index + 1, rightIdx);
+        }
     }
     
     public static int divide(int [] list, int leftIdx, int rightIdx)
     {
-        return 1;
+        if (leftIdx < 0 || leftIdx > list.length - 1 
+                || rightIdx < 0 || rightIdx > list.length - 1) 
+        {
+            throw new IllegalArgumentException("Left or right index invalid.");
+        }
+        
+        int pivotItem = list[rightIdx];
+        int i = leftIdx;
+        int j = rightIdx - 1;
+        
+        while (i <= j) 
+        {
+            while (list[i] <= pivotItem)
+            {
+                i++;
+            }
+            
+            while (list[j] >= pivotItem) 
+            {
+                j--;
+            }
+            // As long as leftIdx have not overstep rightIdx ...
+            if (i <= j) 
+            {
+                int tmp = list[i];
+                
+                list[i] = list[j];
+                list[j] = tmp;
+                
+                i++;
+                j--;
+            }
+        }
+        
+        int tmp = list[j + 1];
+        list[j + 1] = pivotItem;
+        list[rightIdx] = tmp;
+        
+        return j + 1;
     }
     
+    // Exchange the array-elements i and j.
+    // Parameter validity checks required.
     public static void swap(int[] list, int i, int j)
     {
         if (i < 0 || i > list.length - 1 
@@ -86,6 +155,8 @@ public class Sort
         list[j] = tmp;
     }
     
+    // Convert the array to a appropriate String representation!
+    // Parameter validity checks required.
     public static String toString(int [] list, int start, int end)
     {
         if (start < 0 || start > list.length - 1 
@@ -99,10 +170,10 @@ public class Sort
         
         for (int i = start; i <= end; i++) 
         {
-            ret += list[i] + "\n";
+            ret += list[i] + ", ";
         }
         
-        return ret.substring(0, ret.length() - 1);
+        return "[ " + ret.substring(0, ret.length() - 2) + " ]";
     }
     
     public static String toString(int [] list)
--- sortingalgorithms/SortingAlgorithms.java
+++ sortingalgorithms/SortingAlgorithms.java
@@ -4,25 +4,17 @@ package sortingalgorithms;
 
 import algorithm.Sort;
 import static java.lang.System.out;
-/**
- *
- * @author michael
- */
+
 public class SortingAlgorithms
 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         int[] test = { 9, 16, 17, 5, 3, 18, 14, 4, 14, 17 };
         
         out.println(Sort.toString(test) + "\n");
         
-        Sort.bubbleUp(test, 2, test.length - 1);
-        
-        //Sort.swap(test, 1, 4);
+        Sort.quickSort(test, 0, test.length - 1);
         
         out.println(Sort.toString(test));
     }
